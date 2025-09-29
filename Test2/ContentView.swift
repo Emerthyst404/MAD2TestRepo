9a3a327410ac1c3be056659f97f0cc4c440fd656
Kalina <kcathcart3611@conestogac.on.ca>
2025-09-29 09:36:03 -0400

    Initial Commit

---
+++ Test2/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Test2
+//
+//  Created by Student on 2025-09-29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Test2/Test2App.swift
@@ -0,0 +1,17 @@
+//
+//  Test2App.swift
+//  Test2
+//
+//  Created by Student on 2025-09-29.
+//
+
+import SwiftUI
+
+@main
+struct Test2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
